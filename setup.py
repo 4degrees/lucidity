108122a38fe9c5bf08c8c74837193e3d512f951d
Martin Pengelly-Phillips <martin@4degrees.ltd.uk>
2013-09-01 14:14:31 +0100

    Support running tests via setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,26 @@
 # :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
 # :license: See LICENSE.txt.
 
+from setuptools.command.test import test as TestCommand
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+
+class PyTest(TestCommand):
+    '''Pytest command.'''
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        '''Import pytest and run.'''
+        import pytest
+        errno = pytest.main(self.test_args)
+        raise SystemExit(errno)
 
 
 setup(
@@ -23,6 +39,10 @@ setup(
     license='Apache License (2.0)',
     long_description=open('README.rst').read(),
     url='https://github.com/4degrees/lucidity',
-    keywords='filesystem, management, templating'
+    keywords='filesystem, management, templating',
+    tests_require=['pytest'],
+    cmdclass={
+        'test': PyTest
+    }
 )
 
