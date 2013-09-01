84e4f0f68fbd43dcce67668a5431f981cd33e12d
Martin Pengelly-Phillips <martin@4degrees.ltd.uk>
2013-09-01 14:44:49 +0100

    Update setup.py with correct version number and requirements.

--- setup.py
+++ setup.py
@@ -26,23 +26,31 @@ class PyTest(TestCommand):
 
 setup(
     name='Lucidity',
-    version='1.0.0dev',
+    version='0.1.0',
     description='Filesystem templating and management.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+    ],
+    keywords='filesystem, management, templating',
+    url='https://github.com/4degrees/lucidity',
+    author='Martin Pengelly-Phillips',
+    author_email='martin@4degrees.ltd.uk',
+    license='Apache License (2.0)',
     packages=[
         'lucidity',
     ],
     package_dir={
         '': 'source'
     },
-    author='Martin Pengelly-Phillips',
-    author_email='martin@4degrees.ltd.uk',
-    license='Apache License (2.0)',
-    long_description=open('README.rst').read(),
-    url='https://github.com/4degrees/lucidity',
-    keywords='filesystem, management, templating',
-    tests_require=['pytest'],
+    install_requires=[
+        'regex >= 2.4.26',
+        'bunch >= 1.0.1'
+    ],
+    tests_require=['pytest >= 2.3.5'],
     cmdclass={
         'test': PyTest
-    }
+    },
+    zip_safe=False
 )
 
